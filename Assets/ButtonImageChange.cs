2a9e29ed57be2991b06ccfc45b8d066ddfb1a4f1
bach <67271736+Bachnd23@users.noreply.github.com>
2024-06-30 21:20:25 +0700

    button pressed

---
+++ Assets/ButtonImageChange.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonImageChange : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Image buttonImage; // Reference the Image component
+    public Sprite normalImage;
+    public Sprite pressedImage;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonImage.sprite = pressedImage;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonImage.sprite = normalImage;
+    }
+}
