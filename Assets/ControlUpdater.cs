d6c6ce0e4995e1aa629e8474e2a7b8c71533d040
bach <67271736+Bachnd23@users.noreply.github.com>
2024-07-13 17:52:12 +0700

    Check duplicate binding fix

--- Assets/ControlUpdater.cs
+++ Assets/ControlUpdater.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 using static UnityEngine.EventSystems.EventTrigger;
 using TMPro;
 using System;
+using System.Collections.Generic;
 
 public class ControlUpdater : MonoBehaviour, IPointerClickHandler
 {
@@ -14,17 +15,34 @@ public class ControlUpdater : MonoBehaviour, IPointerClickHandler
 
     private InputAction action;
     private TextMeshProUGUI buttonText;
-    public string compositeName ; // Name of the composite binding
-    public string bindingName ; // Name of the specific binding to change
+    public string compositeName; // Name of the composite binding
+    public string bindingName; // Name of the specific binding to change
 
     public Vector2 MoveInput { get; private set; }
     public bool fireTriggered { get; private set; }
     public bool jumpTriggered { get; private set; } // Add this line
+
     private void Start()
     {
         action = playerControls.FindActionMap(actionMapName).FindAction(actionName);
-        RegisterInputActions();
         buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private bool CheckDuplicateBinding(InputAction action, int bindingIndex)
+    {
+        InputBinding newBinding = action.bindings[bindingIndex];
+        int count = 0;
+
+        foreach (InputBinding binding in action.actionMap.bindings)
+        {
+            if (binding.effectivePath == newBinding.effectivePath)
+            {
+                count++;
+            }
+
+        }
+        if(count>1)return true;
+        return false;
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -32,29 +50,35 @@ public class ControlUpdater : MonoBehaviour, IPointerClickHandler
         if (actionName == "Move")
         {
             UpdateControlMove(action);
-        }else 
+        }
+        else
         {
             UpdateControl(action);
         }
     }
+
     void UpdateControlMove(InputAction action)
     {
         // Find the binding index for the specific part of the composite
         int bindingIndex = -1;
         for (int i = 0; i < action.bindings.Count; i++)
         {
-            if (action.bindings[i].isPartOfComposite && action.bindings[i].name==bindingName && action.bindings[i].action == action.name)
+            if (action.bindings[i].isPartOfComposite && action.bindings[i].name == bindingName && action.bindings[i].action == action.name)
             {
                 bindingIndex = i;
                 break;
             }
         }
-        Debug.Log(bindingIndex);
+
         if (bindingIndex == -1)
         {
             Debug.LogError("Binding not found");
             return;
         }
+
+        InputBinding oldBinding = action.bindings[bindingIndex];
+        // Store the original binding path
+        string originalBindingPath = oldBinding.effectivePath;
 
         // Disable the action while rebinding
         action.Disable();
@@ -62,19 +86,29 @@ public class ControlUpdater : MonoBehaviour, IPointerClickHandler
         var rebindOperation = action.PerformInteractiveRebinding(bindingIndex)
             .OnComplete(operation =>
             {
-                // After rebinding, re-enable the action and update the button text
-                action.Enable();
-                // Extract the key from the effective path
-                string fullPath = action.bindings[bindingIndex].effectivePath;
-                string key = fullPath.Substring(fullPath.LastIndexOf('/') + 1).ToUpper();
+                if (CheckDuplicateBinding(action, bindingIndex))
+                {
+                    // Rollback to the original binding path if duplicate is found
+                    action.ApplyBindingOverride(bindingIndex, new InputBinding { overridePath = originalBindingPath });
 
-                buttonText.text = key;
+                    Debug.Log("Duplicate binding found. Rolling back to original binding.");
+                }
+                else
+                {
+                    // Extract the key from the effective path
+                    string fullPath = action.bindings[bindingIndex].effectivePath;
+                    string key = fullPath.Substring(fullPath.LastIndexOf('/') + 1).ToUpper();
+
+                    buttonText.text = key;
+                }
+                // After rebinding, re-enable the action 
+                action.Enable();
                 operation.Dispose();
             });
 
         rebindOperation.Start();
-        RegisterInputActions();
     }
+
     void UpdateControl(InputAction action)
     {
         // Find the binding index for the specific part of the composite
@@ -94,53 +128,35 @@ public class ControlUpdater : MonoBehaviour, IPointerClickHandler
             return;
         }
 
+        InputBinding oldBinding = action.bindings[bindingIndex];
+        // Store the original binding path
+        string originalBindingPath = oldBinding.effectivePath;
         // Disable the action while rebinding
         action.Disable();
 
         var rebindOperation = action.PerformInteractiveRebinding(bindingIndex)
             .OnComplete(operation =>
             {
-                // After rebinding, re-enable the action and update the button text
-                action.Enable();
-                // Extract the key from the effective path
-                string fullPath = action.bindings[bindingIndex].effectivePath;
-                string key = fullPath.Substring(fullPath.LastIndexOf('/') + 1).ToUpper();
+                if (CheckDuplicateBinding(action, bindingIndex))
+                {
+                    // Rollback to the original binding path if duplicate is found
+                    action.ApplyBindingOverride(bindingIndex, new InputBinding { overridePath = originalBindingPath });
 
-                buttonText.text = key;
+                    Debug.Log("Duplicate binding found. Rolling back to original binding.");
+                }
+                else
+                {
+                    // Extract the key from the effective path
+                    string fullPath = action.bindings[bindingIndex].effectivePath;
+                    string key = fullPath.Substring(fullPath.LastIndexOf('/') + 1).ToUpper();
+
+                    buttonText.text = key;
+                }
+                // After rebinding, re-enable the action 
+                action.Enable();
                 operation.Dispose();
             });
 
         rebindOperation.Start();
-        RegisterInputActions();
-    }
-    void RegisterInputActions()
-    {
-        switch (actionName)
-        {
-            case "Move":
-                action.performed += context => MoveInput = context.ReadValue<Vector2>();
-                action.canceled += context => MoveInput = Vector2.zero;
-                break;
-            case "Jump":
-                action.performed += context => jumpTriggered = true;
-                action.canceled += context => jumpTriggered = false;
-                break;
-            case "Fire":
-                 action.performed += context => fireTriggered = true;
-                 action.canceled += context => fireTriggered = false;
-                break;
-                // Add other cases as needed
-        }
-
-        // Example for other actions
-        // lookAction.performed += context => LookInput = context.ReadValue<Vector2>();
-        // lookAction.canceled += context => LookInput = Vector2.zero;
-
-        // fireAction.performed += context => fireTriggered = true;
-        // fireAction.canceled += context => fireTriggered = false;
-
-        // pauseAction.performed += context => pauseTriggered = true;
-        // pauseAction.canceled += context => pauseTriggered = false;
-
     }
 }
